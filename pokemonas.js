0966eb08f36d21f152acf2f4215b34715242b66a
kecrafxd-stack <kecrafxd@gmai.com>
2026-06-25 17:54:49 -0600

    Pues resulta que la funcion no es

--- pokemonas.js
+++ pokemonas.js
@@ -226,15 +226,25 @@ function StateMovement(atacante, selectordeAtacante, defensor) { //SIN UTILIZAR
     let keyAffect = atacante.stataffect[selectordeAtacante];
 
         if (atacante.whoaffect[selectordeAtacante] == "rival") {
+            //----------------------------------------------------------
+            //Lo que sucede cuando es un debuff para el rival
+            //----------------------------------------------------------
             defensor.statStages[keyAffect + "stage"] += atacante.movestageDelta[selectordeAtacante];
             if ((defensor.statStages[keyAffect + "stage"] >= -6) && (defensor.statStages[keyAffect + "stage"]<= 6)) {
+                
                 defensor.battlestats[keyAffect] = defensor[keyAffect] * 2 / (2 + Math.abs(defensor.statStages[keyAffect + "stage"]));
+                
             } else {
+
                 defensor.statStages[keyAffect + "stage"] -= atacante.movestageDelta[selectordeAtacante];
                 return "No se pueden alterar mas sus stats"; //PROVISIONAL AMORE
+
             }
 
         } else {
+            //----------------------------------------------------------
+            //Lo que sucede cuando es un buff para el usuario
+            //----------------------------------------------------------
             atacante.statStages[keyAffect + "stage"] += atacante.movestageDelta[selectordeAtacante]
             if ((atacante.statStages[keyAffect + "stage"] >= -6) && (atacante.statStages[keyAffect + "stage"] <= 6)) {
                 atacante.battlestats[keyAffect] = atacante[keyAffect] * (2 + atacante.statStages[keyAffect + "stage"]) / 2;
